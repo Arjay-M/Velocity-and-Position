6fada01f5e1382772bc9f9da3e43a2db54349212
Ar-Jay C. <acmacagba1@my.scccd.edu>
2025-11-16 21:47:13 +0000

    velocity and position part 2

---
+++ velAndPos135.m
@@ -0,0 +1,15 @@
+%veolicty and position part 2 - 13.5
+%create time and velocity array
+time = 0:10;
+velocity = [0 2 5 7 9 12 15 18 22 20 17];
+distance = trapz(time,velocity);
+ pos = distance +3;
+fprintf("The object travels a distance of %.1fm during the time.\n",distance)
+fprintf("The final position will be %.1fm\n",pos)
+
+
+
+
+
+
+
